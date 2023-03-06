590ddb6c1a1c3e9f44e2f3864bfa5d62f25e425d
mikzerafa <mikzerafa@hotmail.com>
2023-03-06 12:35:14 +0100

    added pom for roll dice page

---
+++ cypress/support/pom/rollPage.js
@@ -0,0 +1,29 @@
+const get = {
+    betAmountSection: () => cy.get('section[ckass="bet-amount-section"]'),
+    mainCard: () => get.betAmountSection().parent(),
+    betFormSection: () => get.mainCard().find('div[class="bet-form"]'),
+    sliderSection: () => get.mainCard().find('div[class="uo-range"]'),
+    rollDiceSection: () => get.mainCard().find('section[class="wager-footer"]'),
+
+    sprayButton: () => cy.get('button[data-test="mode-batch"]'),
+    autoButton: () => cy.get('button[data-test="mode-auto"]'),
+    slowRollButton: () => cy.get('button[data-test="mode-animation"]'),
+
+    betValue: () => get.betAmountSection().find('input[id="mat-input-4"]'),
+    clearButton: () => get.betAmountSection().find('button[data-test="clear"]'),
+    betplus1Button: () => get.betAmountSection().find('button[data-test="plus-1"]'),
+    betplus10Button: () => get.betAmountSection().find('button[data-test="plus-10"]'),
+    betHalfButton: () => get.betAmountSection().find('button[data-test="1-div-2"]'),
+    betTimes2Button: () => get.betAmountSection().find('button[data-test="x2"]'),
+    betMaxButton: () => get.betAmountSection().find('button[data-test="profit-on-win"]'),
+    switchButton: () => get.betFormSection().find('button[data-test="choice-switch"]'),
+    thresholdField: () => get.betFormSection().find('input[data-test="treshold"]'),
+    multiplierField: () => get.betFormSection().find('input[data-test="multiplier"]'),
+    chanceField: () => get.betFormSection().find('input[data-test="chance"]'),
+    slider: () => get.sliderSection().find('div[style*="var(--success)"'),
+    RollDiceButton: () => get.rollDiceSection().find('button[type="submit"]')
+}
+
+export default {
+    get
+}
